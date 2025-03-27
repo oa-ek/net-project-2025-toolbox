cae2987340f1babf0206058d5327adf9ae94ffb5
DavydShmyhelskyi <shmyhelskyidavid58@gmail.com>
2025-03-27 12:51:54 +0200

    Update

---
+++ UIinterface/UIinterface.Client/ServiceClient/BataryService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Json;
+using Core.DTOs;
+
+
+public class BataryService
+{
+    private readonly HttpClient _httpClient;
+
+    public BataryService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<BataryDto>> GetAllAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<BataryDto>>("api/batary");
+    }
+
+    public async Task<BataryDto> GetByIdAsync(int id)
+    {
+        return await _httpClient.GetFromJsonAsync<BataryDto>($"api/batary/{id}");
+    }
+
+    public async Task AddAsync(BataryDto batary)
+    {
+        await _httpClient.PostAsJsonAsync("api/batary", batary);
+    }
+
+    public async Task UpdateAsync(int id, BataryDto batary)
+    {
+        await _httpClient.PutAsJsonAsync($"api/batary/{id}", batary);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"api/batary/{id}");
+    }
+}
+
+
