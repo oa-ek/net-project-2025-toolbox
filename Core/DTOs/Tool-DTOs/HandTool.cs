101bd847a65e0630774b8fe7744200b944867512
Bohdan Olenin <bohdan.olenin@oa.edu.ua>
2025-04-15 23:45:59 +0300

    add crud page for Hand Tool

--- Core/DTOs/Tool-DTOs/HandTool.cs
+++ Core/DTOs/Tool-DTOs/HandTool.cs
@@ -3,15 +3,11 @@
     public class HandToolDto
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public int BrandId { get; set; }
         public int ConditionId { get; set; }
         public int ToolTypeId { get; set; }
-        public int? LastWorkerId { get; set; }
-        public int LastLocationId { get; set; }
         public double Price { get; set; }
-
-        public string BrandName { get; set; }
-        public string ConditionName { get; set; }
-        public string ToolTypeName { get; set; }
     }
 }
+
