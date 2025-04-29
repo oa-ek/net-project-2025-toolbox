e64de2001c5485f2e38ef773cdfff3104f42df0f
DavydShmyhelskyi <shmyhelskyidavid58@gmail.com>
2025-04-29 16:15:29 +0300

    Revert "Added Styles & CRUD to UIupdated (no styles for manage)"
    This reverts commit 62c8245d9eeb4ea2226c88c0861f89e084a9d0d0.

--- UIupdated/Program.cs
+++ UIupdated/Program.cs
@@ -1,17 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Components.Authorization;
 using UIupdated.Components;
 using UIupdated.Components.Account;
 using UIupdated.Data;
-using Core;
-using Core.DTOs;
-using Core.Mappings;
-using Repository;
-using UIinterface.Services; // Або UIupdated.Services, якщо перенесеш туди
-using System;
-using UIinterface.Components.Account;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace UIupdated
 {
@@ -21,81 +13,22 @@ namespace UIupdated
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // --- Сервіси та базові сервіси ---
-            builder.Services.AddScoped<IBaseService<BrandDto>, BaseService<Brand, BrandDto>>();
-            builder.Services.AddScoped<IBaseService<BataryDto>, BaseService<Batary, BataryDto>>();
-            builder.Services.AddScoped<IBaseService<BataryModelDto>, BaseService<BataryModel, BataryModelDto>>();
-            builder.Services.AddScoped<IBaseService<BossDto>, BaseService<Boss, BossDto>>();
-            builder.Services.AddScoped<IBaseService<ConditionDto>, BaseService<Condition, ConditionDto>>();
-            builder.Services.AddScoped<IBaseService<HandToolDto>, BaseService<HandTool, HandToolDto>>();
-            builder.Services.AddScoped<IBaseService<LocationDto>, BaseService<Location, LocationDto>>();
-            builder.Services.AddScoped<IBaseService<PositionDto>, BaseService<Position, PositionDto>>();
-            builder.Services.AddScoped<IBaseService<PowerSupplyTypeDto>, BaseService<PowerSupplyType, PowerSupplyTypeDto>>();
-            builder.Services.AddScoped<IBaseService<PowerToolDto>, BaseService<PowerTool, PowerToolDto>>();
-            builder.Services.AddScoped<IBaseService<SystemAdminDto>, BaseService<SystemAdmin, SystemAdminDto>>();
-            builder.Services.AddScoped<IBaseService<ToolModelDto>, BaseService<ToolModel, ToolModelDto>>();
-            builder.Services.AddScoped<IBaseService<ToolTypeDto>, BaseService<ToolType, ToolTypeDto>>();
-            builder.Services.AddScoped<IBaseService<WorkerDto>, BaseService<Worker, WorkerDto>>();
+            // Add services to the container.
+            builder.Services.AddRazorComponents();
 
-            // --- Конкретні сервіси ---
-            builder.Services.AddScoped<LocationService>();
-            builder.Services.AddScoped<BossService>();
-            builder.Services.AddScoped<SystemAdminService>();
-            builder.Services.AddScoped<WorkerService>();
-            builder.Services.AddScoped<PositionService>();
-            builder.Services.AddScoped<HandToolService>();
-            builder.Services.AddScoped<PowerToolService>();
-            builder.Services.AddScoped<PowerSupplyTypeService>();
-            builder.Services.AddScoped<BrandService>();
-            builder.Services.AddScoped<BataryService>();
-            builder.Services.AddScoped<BataryModelService>();
-            builder.Services.AddScoped<ConditionService>();
-            builder.Services.AddScoped<ToolModelService>();
-            builder.Services.AddScoped<ToolTypeService>();
-
-            // --- Репозиторії ---
-            builder.Services.AddScoped<BaseRepository<Brand>>();
-            builder.Services.AddScoped<BaseRepository<Batary>>();
-            builder.Services.AddScoped<BaseRepository<BataryModel>>();
-            builder.Services.AddScoped<BaseRepository<Boss>>();
-            builder.Services.AddScoped<BaseRepository<Condition>>();
-            builder.Services.AddScoped<BaseRepository<HandTool>>();
-            builder.Services.AddScoped<BaseRepository<Location>>();
-            builder.Services.AddScoped<BaseRepository<Position>>();
-            builder.Services.AddScoped<BaseRepository<PowerSupplyType>>();
-            builder.Services.AddScoped<BaseRepository<PowerTool>>();
-            builder.Services.AddScoped<BaseRepository<SystemAdmin>>();
-            builder.Services.AddScoped<BaseRepository<ToolModel>>();
-            builder.Services.AddScoped<BaseRepository<ToolType>>();
-            builder.Services.AddScoped<BaseRepository<Worker>>();
-            builder.Services.AddScoped<RepositoryContainer>();
-
-            // AutoMapper
-            builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-            // Razor Components
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
             builder.Services.AddCascadingAuthenticationState();
             builder.Services.AddScoped<IdentityUserAccessor>();
             builder.Services.AddScoped<IdentityRedirectManager>();
 
-            // Authentication
             builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-            }).AddIdentityCookies();
-
+                {
+                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+                })
+                .AddIdentityCookies();
             builder.Services.AddAuthorization();
 
-            // DbContexts
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-            builder.Services.AddDbContext<TTContext>(options =>
-                options.UseSqlServer(connectionString));
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -107,19 +40,33 @@ namespace UIupdated
                 .AddDefaultTokenProviders();
 
             builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            // Google Auth
-            builder.Services.AddAuthentication().AddGoogle(options =>
-            {
-                options.ClientId = builder.Configuration["Google:ClientId"];
-                options.ClientSecret = builder.Configuration["Google:ClientSecret"];
-                options.CallbackPath = "/signin-google";
-            });
+          
+            // GOOGLE AUTH
 
-            // App pipeline
+            builder.Services.AddAuthentication()
+                 .AddGoogle(options =>
+                 {
+                     options.ClientId = builder.Configuration["Google:ClientId"];
+                     options.ClientSecret = builder.Configuration["Google:ClientSecret"];
+                     options.CallbackPath = "/signin-google"; // ??????? ??? ?????, ???? ??? ?????????
+                 });
+
+
+//            // налаштування Identity + EmailSender
+//            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+//            {
+//                options.SignIn.RequireConfirmedEmail = true; // ?? важливо
+//            })
+//.AddEntityFrameworkStores<ApplicationDbContext>();
+
+//            builder.Services.AddTransient<IEmailSender, SmtpEmailSender>(); // ?? Реалізуємо далі
+
+
+
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -127,16 +74,18 @@ namespace UIupdated
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
             app.UseAntiforgery();
-            app.UseAuthentication();
-            app.UseAuthorization();
 
             app.MapStaticAssets();
-            app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+            app.MapRazorComponents<App>();
+
+            // Add additional endpoints required by the Identity /Account Razor components.
             app.MapAdditionalIdentityEndpoints();
 
             app.Run();
