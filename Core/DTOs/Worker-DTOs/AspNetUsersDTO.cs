12391bc4687301cabd2e622f9f223d14dc0ad583
DavydShmyhelskyi <shmyhelskyidavid58@gmail.com>
2025-05-22 23:38:28 +0300

    Added asiging roles to workers

---
+++ Core/DTOs/Worker-DTOs/AspNetUsersDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DTOs
+{
+        public class UserDto
+        {
+            public string Id { get; set; }               // Primary key
+            public string FirstName { get; set; }        // Ім’я
+            public string LastName { get; set; }         // Прізвище
+            public string UserName { get; set; }         // Логін
+            public string Email { get; set; }            // Електронна пошта
+            public bool EmailConfirmed { get; set; }     // Чи підтверджена пошта
+            public bool IsWorker { get; set; }           // Чи має роль "Worker" (не з таблиці напряму, а обчислюється)
+        }
+}
